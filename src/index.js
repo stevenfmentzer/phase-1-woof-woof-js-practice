fbb7dff48adf048450928594defbc4959b4f04f7
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-28 13:38:21 -0500

    pessimistic rendering of good dog filter

--- src/index.js
+++ src/index.js
@@ -2,76 +2,72 @@
 const dogBarElement = document.getElementById("dog-bar")
 const dogInfoElement = document.getElementById("dog-info")
 const goodDogFilter = document.getElementById("good-dog-filter")
-let filterStatus = false
-
-const dogNameElement = document.createElement("h2")
-dogInfoElement.appendChild(dogNameElement)
 const dogImageElement = document.createElement("img")
-dogInfoElement.appendChild(dogImageElement)
+const dogNameElement = document.createElement("h2")
 const goodDogButtonElement = document.createElement("button")
-dogInfoElement.appendChild(goodDogButtonElement)
+dogInfoElement.append(dogImageElement, dogNameElement, goodDogButtonElement)
+
+let filterStatus = false
 let currentDog
 
 function renderDog(dog){
     const dogSpan = document.createElement("span")
     dogSpan.isGoodDog = dog.isGoodDog
     dogSpan.textContent = dog.name
+    dogSpan.id = dog.name
     dogBarElement.appendChild(dogSpan)
     dogSpan.addEventListener("click", () => {
         displayDog(dog)
-    })
-}
+})}
 
 function displayDog(dog){
-    dogNameElement.textContent = dog.name
+    dogInfoElement.style.display = ""
+    dogNameElement.innerHTML = dog.name
     dogImageElement.src = dog.image 
-    goodDogButtonElement.textContent = goodDogButton(dog.isGoodDog)
+    goodDogButtonElement.innerHTML = dog.isGoodDog? "Good Dog!" : "Bad Dog!"
     currentDog = dog
 }
 
-function goodDogButton(bool){
-    if (bool){return "Good Dog!"} 
-    else {return "Bad Dog!"} 
-}
-
-function setFilter(bool){
-    if (bool){return "Filter good dogs: ON"} 
-    else {return "Filter good dogs: OFF"} 
-}
-
 goodDogButtonElement.addEventListener("click", () => {
-    if (currentDog.isGoodDog){currentDog.isGoodDog = false} 
-    else {currentDog.isGoodDog = true}
+    currentDog.isGoodDog = !currentDog.isGoodDog
+    goodDogButtonElement.textContent = currentDog.isGoodDog? "Good Dog!" : "Bad Dog!"
+    let dogSpan = document.getElementById(currentDog.name)
+    dogSpan.isGoodDog = currentDog.isGoodDog
+    let updatingData = {"isGoodDog" : currentDog.isGoodDog}
+    patchDog(currentDog.id,updatingData)
+    if (filterStatus && !currentDog.isGoodDog){
+        dogSpan.style.display = "none"
+        dogInfoElement.style.display = "none"
+}})
 
-    fetch(`http://localhost:3000/pups/${currentDog.id}`, {
+function patchDog(url, updatingData){
+    fetch(`http://localhost:3000/pups/${url}`, {
         "method" : "PATCH",
         "headers" : {"Content-Type" : "application/json"},
-        "body" : JSON.stringify({"isGoodDog" : currentDog.isGoodDog})
-    })
-    .then(response => response.json())
-
-    goodDogButtonElement.textContent = goodDogButton(currentDog.isGoodDog)
-})
+        "body" : JSON.stringify(updatingData)
+})}
 
 goodDogFilter.addEventListener("click", () => {
-    if (filterStatus){filterStatus = false} 
-    else {filterStatus = true}
-        
-    goodDogFilter.textContent = setFilter(filterStatus)
+    filterStatus = !filterStatus
+    goodDogFilter.textContent = filterStatus? "Filter good dogs: ON" : "Filter good dogs: OFF"
+    const dogSpanArray = dogBarElement.getElementsByTagName("span")
 
-    const dogSpans = dogBarElement.getElementsByTagName("span")
-    for (let i = 0 ; i < dogSpans.length ; i++){  
-        const dog = dogSpans[i]
+    for (let i = 0 ; i < dogSpanArray.length ; i++){  
+        const dog = dogSpanArray[i]
         if (filterStatus === true && dog.isGoodDog === false){
             dog.style.display = "none"
-        } else dog.style.display = ""
-    }})
+        } else { 
+            dog.style.display = ""
+        }}
+    if (currentDog.isGoodDog === false){
+        dogInfoElement.style.display = "none"
+    }
+})
 
 fetch ("http://localhost:3000/pups")
 .then(response => response.json())
 .then((dogList) => dogList.forEach((dog) => {
     renderDog(dog)
     displayDog(dogList[0])
-    setFilter(false)
     })
 )
